0c9e20ff3f3eccacfe664ef7b14b3613e2f9f04f
A.MOHAMMED ASBAN ZIA <asbanzia0205@example.com>
2026-05-19 23:58:52 +0530

    Firebase setup completed

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnN7X-sQ4-WeSs--jHlc_HMgxztQjgTPg",
+  authDomain: "tamil-nadu-wholesale-market.firebaseapp.com",
+  projectId: "tamil-nadu-wholesale-market",
+  storageBucket: "tamil-nadu-wholesale-market.firebasestorage.app",
+  messagingSenderId: "882793402361",
+  appId: "1:882793402361:web:923ba2e0c23b9458a53728"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
